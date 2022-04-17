c7a69654311e22886e4f084e4c47d7fcb9c29a39
Isabel boaventura <boaventuraisabel31@gmail.com>
2022-04-17 19:11:02 -0300

    Delete Celke directory

--- Celke/02/js/datatable.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#listar-cargos').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": "scripts/server_processing.php"
-    });
-} );
